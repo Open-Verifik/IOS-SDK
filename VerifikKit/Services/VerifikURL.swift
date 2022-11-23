75cf7ba4483ddf5a34fef7bbc948378f5fa5ebfc
carlos bleck <charliesbleck@gmail.com>
2022-11-23 23:54:49 +0000

    Verifik init

---
+++ VerifikKit/Services/VerifikURL.swift
@@ -0,0 +1,18 @@
+//
+//  VerifikURL.swift
+//  VerifikKit
+//
+//  Created by Carlos Bleck on 02/11/22.
+//
+
+import Foundation
+
+class VerifikURL{
+    static let Base = "https://staging-api.verifik.co/v2/biometrics"
+    static let Credentials = "/config"
+    static let Session = "/session"
+    static let Enroll = "/enroll"
+    static let Authenticate = "/authenticate"
+    static let MatchID = "/match-idscan"
+    static let IDScan = "/idscan"
+}
